d07672a7a4db0eb39558fd89473f700600b6c4d9
yeeun <dpdmstjs@gmail.com>
2023-06-28 21:45:31 +0900

    Refactor: 포지션별 목록 조회 position 변경
    - Position enum 사용

--- src/main/java/BaseBallApp.java
+++ src/main/java/BaseBallApp.java
@@ -5,113 +5,60 @@ import java.net.URL;
 import java.sql.Connection;
 
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import constant.Position;
 import dao.PositionDao;
 import dao.TeamDAO;
 import db.DBConnection;
 import dao.StadiumDAO;
 import dto.PositionRespDto;
+import util.ComponentScan;
+import util.MethodInfo;
+import util.MyScanner;
+import util.annotation.RequestMapping;
 
 public class BaseBallApp {
-	public static void main(String[] args) throws SQLException {
+
+	//@TODO: 별도 App 클래스 만들어서 메인 메서드에서는 App.run(); 방식으로 구조 변경
+	public static void main(String[] args) throws Exception {
 		Connection connection = DBConnection.getInstance();
-		StadiumDAO stadiumDAO = new StadiumDAO(connection);
-		TeamDAO teamDAO = new TeamDAO(connection);
 		PositionDao positionDao = new PositionDao(connection);
 
 		PositionRespDto positionRespDto = positionDao.positionList();
+		Map<Position, List<String>> positionMap = positionRespDto.getPositionMap();
+		List<String> teamList = positionRespDto.getTeamList();
 
-		Map<String, List<String>> positionMap = positionRespDto.getPositionMap();
-		System.out.printf("%-10s", "Position");
-		positionMap.keySet().forEach(team -> System.out.printf("%-10s", team));
+		System.out.printf("%-10s", "포지션");
+		for (String team : teamList) {
+			System.out.printf("%-10s", team);
+		}
 		System.out.println();
-		positionMap.forEach((position, teamPlayerList) -> {
-			System.out.printf("%-10s", position);
-			teamPlayerList.forEach(player -> System.out.printf("%-10s", player));
+
+		for (Position position : positionMap.keySet()) {
+			System.out.printf("%-10s", position.getName());
+			List<String> teamPlayerList = positionMap.get(position);
+			for (String team : teamList) {
+				String playerName = teamPlayerList.get(teamList.indexOf(team));
+				System.out.printf("%-10s", playerName);
+			}
 			System.out.println();
-		});
-    
+		}
+
 		MyScanner scanner = new MyScanner();
+		ComponentScan componentScan = ComponentScan.getInstance();
 
 		while (true) {
 			String uri = scanner.getRequest();
 			MethodInfo methodInfo = scanner.parseData(uri);
-			Set<Class> classes = componentScan("service");
-			findUri(classes, methodInfo);
+			Set<Class> classes = componentScan.scanPackage("controller");
+			String response = componentScan.findUri(classes, methodInfo);
+
+			System.out.println(response);
 		}
 
-	}
-
-	public static Set<Class> componentScan(String pkg) throws Exception {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		Set<Class> classes = new HashSet<>();
-
-		URL packageUrl = classLoader.getResource(pkg);
-		File packageDirectory = new File(packageUrl.toURI());
-		for (File file : packageDirectory.listFiles()) {
-			if (file.getName().endsWith(".class")) {
-				String className = pkg + "." + file.getName().replace(".class", "");
-				Class cls = Class.forName(className);
-				classes.add(cls);
-			}
-		}
-		return classes;
-	}
-
-	public static void findUri(Set<Class> classes, MethodInfo methodInfo) throws Exception {
-		boolean isFind = false;
-		for (Class cls : classes) {
-			if (cls.isAnnotationPresent(Service.class)) {
-				Object instance = cls.newInstance();
-				Method[] methods = cls.getDeclaredMethods();
-
-				for (Method method : methods) {
-					Annotation annotation = method.getDeclaredAnnotation(RequestMapping.class);
-					RequestMapping requestMapping = (RequestMapping)annotation;
-
-					if (requestMapping == null)
-						continue;
-
-					if (requestMapping.name().equals(methodInfo.getName())) {
-						isFind = true;
-						method.setAccessible(true);
-
-						try {
-							if (method.getParameterTypes().length == 0) {
-								method.invoke(instance);
-								break;
-							}
-
-							Class<?>[] parameterTypes = method.getParameterTypes();
-							Object[] arg = new Object[parameterTypes.length];
-							for (int i = 0; i < parameterTypes.length; i++) {
-								arg[i] = convertArgumentType(methodInfo.getParameters()[i], parameterTypes[i]);
-							}
-							method.invoke(instance, arg);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	private static Object convertArgumentType(Object arg, Class<?> targetType) {
-		// 타입에 따라 적절한 변환 로직을 구현한다
-		if (targetType.equals(int.class) || targetType.equals(Integer.class)) {
-			return Integer.parseInt(arg.toString());
-		} else if (targetType.equals(String.class)) {
-			return arg.toString();
-		} else if (targetType.equals(boolean.class) || targetType.equals(Boolean.class)) {
-			return Boolean.parseBoolean(arg.toString());
-		}
-		// 추가적인 타입 변환 로직을 구현한다
-
-		return arg;
 	}
 }
--- src/main/java/dao/PositionDao.java
+++ src/main/java/dao/PositionDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class PositionDao {
 	private final Connection connection;
 
+
 	public PositionRespDto positionList() {
 		List<String> teamList = getTeamNameList();
-		Map<String, List<String>> positionMap = new HashMap<>();
+		Map<Position, List<String>> positionMap = new HashMap<>();
 
 		try {
 			StringBuilder builder = new StringBuilder();
@@ -49,7 +51,7 @@ public class PositionDao {
 
 			try (ResultSet resultSet = statement.executeQuery()) {
 				while (resultSet.next()) {
-					String position = resultSet.getString("position");
+					Position position = Position.findByName(resultSet.getString("position"));
 					List<String> teamPlayerList = new ArrayList<>();
 
 					for (String team : teamList) {
@@ -64,6 +66,7 @@ public class PositionDao {
 		}
 		return PositionRespDto.builder()
 			.positionMap(positionMap)
+			.teamList(teamList)
 			.build();
 	}
 
@@ -75,38 +78,10 @@ public class PositionDao {
 			try (ResultSet resultSet = statement.executeQuery()) {
 				while (resultSet.next()) {
 					teamList.add(resultSet.getString("name"));
-          
-	public List<PositionRespDto> getPositionInfo() throws SQLException {
-		List<PositionRespDto> positionList = new ArrayList<>();
-
-		String query1 =
-			"SELECT GROUP_CONCAT(DISTINCT CONCAT('GROUP_CONCAT(CASE WHEN t.name = ''', t.name, ''' THEN p.name ELSE NULL END) AS `', t.name, '`') SEPARATOR ',') "
-				+
-				"FROM player p JOIN team t ON p.team_id = t.id";
-
-		try (PreparedStatement statement1 = connection.prepareStatement(
-			query1); ResultSet resultSet1 = statement1.executeQuery()) {
-			resultSet1.next();
-
-			String teams = resultSet1.getString(1);
-
-			String query2 = "SELECT p.position," + teams +
-				"FROM player p JOIN team t ON p.team_id = t.id " +
-				"GROUP BY p.position";
-
-			try (PreparedStatement statement2 = connection.prepareStatement(
-				query2); ResultSet resultSet2 = statement2.executeQuery()) {
-				while (resultSet2.next()) {
-					String teamPlayers = resultSet2.getString(2);
-					List<String> teamPlayerList = Arrays.asList(teamPlayers.split(","));
-					PositionRespDto positionDto = PositionRespDto.builder()
-						.position(Position.findByName(resultSet2.getString("position")))
-						.teamPlayers(teamPlayerList)
-						.build();
-					positionList.add(positionDto);
 				}
 			}
-			return positionList;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 		return teamList;
 	}
